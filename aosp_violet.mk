733527ac9a4f3535e280f691de4aeaab9bf79da9
Ujjwal Kirorimal <ujjwalkirorimal@gmail.com>
2020-04-27 19:11:20 +0530

    violet: Address Anxcam Denials * Also make anxcam optional to build

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -35,4 +35,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Miui_Camera
-$(call inherit-product, vendor/MiuiCamera/config.mk)
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
