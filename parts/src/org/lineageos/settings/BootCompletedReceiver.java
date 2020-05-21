a26690a82c86b62eb4e6e9577001a767d2a1e917
Pranav Vashi <neobuddy89@gmail.com>
2020-05-21 23:43:29 +0530

    violet: Add switchbar to disable dynamic thermal
    * Disabled by default since all profile are set to 0 by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I7fc8c5d82072a5388ebdd8d3ac679ec08c9cc023

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -33,11 +33,12 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-            ThermalUtils.startService(context);
+            ThermalUtils.initialize(context);
         if (DozeUtils.isDozeEnabled(context) && DozeUtils.sensorsEnabled(context)) {
             if (DEBUG) Log.d(TAG, "Starting Doze service");
             DozeUtils.startService(context);
         }
+
     }
 
 }
